1db52bdcbd44cdd390618d4c3bec9bae0df9761b
Rinduyuna <36070215+Rinduyuna@users.noreply.github.com>
2018-03-17 22:22:01 +0700

    Add files via upload

---
+++ generated/source/buildConfig/debug/com/example/rinduyuna/rinduyunaputri_1202150286_modul4/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.rinduyuna.rinduyunaputri_1202150286_modul4;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.rinduyuna.rinduyunaputri_1202150286_modul4";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
